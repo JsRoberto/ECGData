dd7e6d90af29a80c4cfd239401ea5374ce27f7be
José Roberto Sobrinho Lima <jsroberto.slima@gmail.com>
2016-10-06 15:27:25 -0300

    Add files via upload

--- projetoECG.R
+++ projetoECG.R
@@ -3,6 +3,18 @@ library(ggplot2)
 
 urlFile <- c("https://raw.githubusercontent.com/JsRoberto/ECGData/master/mitdb_ecgSignals.csv",
              "https://raw.githubusercontent.com/JsRoberto/ECGData/master/fs.csv")
-
 localFile <- c("./mitdb_ecgSignals.csv","./fs.csv")
+
+download <- function(urlFile, localFile) {
+      if (!exists(localFile)) {
+            download.file(urlFile,localFile)
+      }
+}
+
+mapply(download, urlFile, localFile)
+
+dataECG <- read.csv(localFile[1], header = TRUE)
+dataECG.split <- split(dataECG, dataECG$signal_case)
+fs <- read.csv(localFile[2], header = TRUE)
+fs <- as.numeric(fs)
 
